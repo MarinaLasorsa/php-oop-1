a51422d7e78f1c4b0e11a78720437150caab2811
MarinaLasorsa <marina.lasorsa@gmail.com>
2024-05-09 16:47:49 +0200

    FIXED: moved functions inside the class

--- index.php
+++ index.php
@@ -4,18 +4,20 @@ class Production {
     public $title;
     public $language;
     public $vote;
-}
 
-public function setVote(int $vote) {
-    if (is_numeric($vote) && $vote > 0 $$ $vote <= 10) {
-        $this->vote = intval($vote);
-    } else {
-        var_dump('errore');
+
+    public function setVote(int $vote) {
+        if (is_numeric($vote) && $vote > 0 $$ $vote <= 10) {
+            $this->vote = intval($vote);
+        } else {
+            var_dump('errore');
+        }
     }
-}
 
-function __construct (string $_title, string $_language, int $_vote) {
-    $this->title = $_title;
-    $this->language = $_language;
-    $this->setVote($_vote);
+    function __construct (string $_title, string $_language, int $_vote) {
+        $this->title = $_title;
+        $this->language = $_language;
+        $this->setVote($_vote);
+    }
+
 }
