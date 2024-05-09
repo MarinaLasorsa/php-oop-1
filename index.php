dc9aefbb174388aedcf11b74b4e209f4f7dca716
MarinaLasorsa <marina.lasorsa@gmail.com>
2024-05-09 16:37:33 +0200

    ADD: method to set the vote attribute + construct function

--- index.php
+++ index.php
@@ -5,3 +5,17 @@ class Production {
     public $language;
     public $vote;
 }
+
+public function setVote(int $vote) {
+    if (is_numeric($vote) && $vote > 0 $$ $vote <= 10) {
+        $this->vote = intval($vote);
+    } else {
+        var_dump('errore');
+    }
+}
+
+function __construct (string $_title, string $_language, int $_vote) {
+    $this->title = $_title;
+    $this->language = $_language;
+    $this->setVote($_vote);
+}
